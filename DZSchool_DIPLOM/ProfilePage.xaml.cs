af94b4c61a8a6ecb814e8925d87517752c7342ec
Rainbow Dash <arbuzz.tf2.ru@yandex.ru>
2023-05-09 23:26:57 +0800

    1

--- DZSchool_DIPLOM/ProfilePage.xaml.cs
+++ DZSchool_DIPLOM/ProfilePage.xaml.cs
@@ -29,29 +29,46 @@ namespace DZSchool_DIPLOM
             PupilFio.Content = pupil.Fio.ToString();
             Lvl.Content = pupil.AvgMarks.ToString();
             lvlbar.Value = Convert.ToDouble(pupil.AvgMarks.ToString());
-            int reward = Convert.ToInt32(pupil.AvgMarks.ToString());
+            double reward = Convert.ToDouble(pupil.AvgMarks.ToString());
+            Image myImage = new Image();
+            myImage.Width = 200;
+            BitmapImage myBitmapImage = new BitmapImage();
+            myBitmapImage.BeginInit();
+
             if (reward <= 2)
             {
-                Reward.Content = "Старайся лучше!";
-                RewardName.Content = "К сожалению ты ничего не получаешь";
-                //ImageReward.Source = "C:\Users\arbuz\source\repos\DZSchool_DIPLOM\res\rewards\nothing.jpg";
+                Reward.Text = "Старайся лучше!";
+                RewardName.Text = "К сожалению ты ничего не получаешь";
+                myBitmapImage.UriSource = new Uri(@"C:\Users\arbuz\source\repos\DZSchool_DIPLOM\res\rewards\nothing.jpg");
+                myBitmapImage.DecodePixelWidth = 200;
+                myBitmapImage.EndInit();
             }
             else
                 if ((reward > 2) && (reward <= 3))
             {
-                Reward.Content = "Ты можешь лучше, но в конце месяца получишь конфетку";
-                RewardName.Content = "Твоя награда - конфетка";
+                Reward.Text = "Ты можешь лучше, но в конце месяца получишь конфетку";
+                RewardName.Text = "Твоя награда - конфетка";
+                myBitmapImage.UriSource = new Uri(@"C:\Users\arbuz\source\repos\DZSchool_DIPLOM\res\rewards\sweet.jpg");
+                myBitmapImage.DecodePixelWidth = 200;
+                myBitmapImage.EndInit();
             }
             else if ((reward > 3) && (reward <= 4))
             {
-                Reward.Content = "Ты молодец, но еще есть куда расти, в конце месяца ты получишь набор для учебы";
-                RewardName.Content = "Твоя награда - набор из: 2 тетради 48 листов, ручка синяя c дизайном на выбор!";
+                Reward.Text = "Ты молодец, но еще есть куда расти, в конце месяца ты получишь набор для учебы";
+                RewardName.Text = "Твоя награда - набор из: 2 тетради 48 листов, ручка синяя c дизайном на выбор!";
+                myBitmapImage.UriSource = new Uri(@"C:\Users\arbuz\source\repos\DZSchool_DIPLOM\res\rewards\copybookANDpen.jpg");
+                myBitmapImage.DecodePixelWidth = 200;
+                myBitmapImage.EndInit();
             }
             else if (reward > 4)
             {
-                Reward.Content = "Ты просто бог учебы, мои поздравления, в конце месяца ты получишь билет в кино или в театр на выбор";
-                RewardName.Content = "Твоя награда - один билет в кино или театр (на твой выбор), и фильм, оперу или балет выбираешь ты! Поздравляю!";
+                Reward.Text = "Ты просто бог учебы, мои поздравления, в конце месяца ты получишь билет в кино или в театр на выбор";
+                RewardName.Text = "Твоя награда - один билет в кино или театр (на твой выбор), и фильм, оперу или балет выбираешь ты! Поздравляю!";
+                myBitmapImage.UriSource = new Uri(@"C:\Users\arbuz\source\repos\DZSchool_DIPLOM\res\rewards\tickets.jpg");
+                myBitmapImage.DecodePixelWidth = 200;
+                myBitmapImage.EndInit();
             }
+            myImage.Source = myBitmapImage;
         }
     }
 }
